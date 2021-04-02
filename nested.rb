d1c79ed0761cb0a2a0a9c524eeae40d667f94e27
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-02 20:26:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,6 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:known_for] << "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 return programmer_hash
 end
